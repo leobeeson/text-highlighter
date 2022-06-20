41586cbc53f966f43789b848531b5d6ac93fc458
Francis Beeson <leobeeson@gmail.com>
2022-06-20 18:39:57 +0100

    RED: Add test with censurable message in proper case and with multi-word terms in blacklist.

--- tests/text_processing/test_highlight_text.py
+++ tests/text_processing/test_highlight_text.py
@@ -124,3 +124,14 @@ def test_highlightable_message_with_uppercase_multiword_term_to_highlight_presen
         TextFragmentWithHighlights("Also,", is_highlighted=False),
         TextFragmentWithHighlights("HELLO WORLD!", is_highlighted=True),
     ])
+
+
+def test_highlightable_message_with_propercase_multiword_term_to_highlight_present_in_message():
+    message = "This is a test message to test the message highlighter. Also, Hello World!"
+    result = highlight_text(message, ["message highlighter.", "hello world!"])
+    assert result == TextWithHighlights([
+        TextFragmentWithHighlights("This is a test message to test the", is_highlighted=False),
+        TextFragmentWithHighlights("message highlighter.", is_highlighted=True),
+        TextFragmentWithHighlights("Also,", is_highlighted=False),
+        TextFragmentWithHighlights("Hello World!", is_highlighted=True),
+    ])
